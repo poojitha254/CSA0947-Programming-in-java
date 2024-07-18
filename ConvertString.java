1479b904a2a490f38da22b30b9a231dc9fc4d180
poojitha254 <123923868+poojitha254@users.noreply.github.com>
2024-07-18 08:44:20 +0530

    Add files via upload

---
+++ ConvertString.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class ConvertString
+{
+	public static void main(String arg[])
+	{
+		Scanner sc = new Scanner(System.in);
+		String input;
+		System.out.print("enter a number in string:");
+		input=sc.nextLine();
+		int number=Integer.parseInt(input);
+		System.out.println("output:"+number);
+	}
+}
